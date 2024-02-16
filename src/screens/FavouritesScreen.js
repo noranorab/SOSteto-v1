ac435313b86670cb5bf9baf601e6651a91ce6c06
noranorab <noranorab7@gmail.com>
2024-02-16 10:48:20 +0100

    added footer

---
+++ src/screens/FavouritesScreen.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { View, Text} from 'react-native';
+
+
+export default function FavouritesScreen({navigation}) {
+    return(
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <Text 
+            onPress = {() => alert('This is the favourites screen.')}
+            style={{ fontSize: 26, fontWeight: 'bold'}}>
+                Public Profile
+            </Text>
+        </View>
+    )
+}
