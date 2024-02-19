ed3bdbc074912c55365e015613f910f1a986c0da
MeriemBelh <meriem.balhaddad2001@gmail.com>
2024-02-19 23:36:21 +0100

    ajouter

--- src/screens/AjouterScreen.js
+++ src/screens/AjouterScreen.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { View, Text} from 'react-native';
 
 
-export default function MessagesScreen({navigation}) {
+export default function AjouterScreen({navigation}) {
     return(
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
             <Text 
