fdcfa26e3aa1a894f949aca3e3c2acfeecd64f31
noranorab <noranorab7@gmail.com>
2024-02-19 23:49:07 +0100

    added source image link

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,13 +1,13 @@
 import * as React from 'react';
-import { View, Text, Image, FlatList} from 'react-native';
-import { Button } from 'react-native-web';
+import { View, Text, Image, FlatList, Button} from 'react-native';
+
 
 
 const posts = [
-    {id: 1, postOwner: 'Mohammed Boulija', ownerBio: "Infirmier Puériculteur Diplômé d'état", location: 'Rabat, Maroc'},
-    {id: 2, postOwner: 'Mohammed Boulija', ownerBio: "Infirmier Puériculteur Diplômé d'état", location: 'Rabat, Maroc'},
-    {id: 3, postOwner: 'Mohammed Boulija', ownerBio: "Infirmier Puériculteur Diplômé d'état", location: 'Rabat, Maroc'},
-    {id: 4, postOwner: 'Mohammed Boulija', ownerBio: "Infirmier Puériculteur Diplômé d'état", location: 'Rabat, Maroc'}
+    {id: 1, postOwner: 'Mohammed Boulija', ownerBio: "Infirmier Puériculteur Diplômé d'état", location: 'Rabat, Maroc', sourceImageProfile: '../../assets/flower.jpg'},
+    {id: 2, postOwner: 'Mohammed Boulija', ownerBio: "Infirmier Puériculteur Diplômé d'état", location: 'Rabat, Maroc', sourceImageProfile: '../../assets/flower.jpg'},
+    {id: 3, postOwner: 'Mohammed Boulija', ownerBio: "Infirmier Puériculteur Diplômé d'état", location: 'Rabat, Maroc', sourceImageProfile: '../../assets/flower.jpg'},
+    {id: 4, postOwner: 'Mohammed Boulija', ownerBio: "Infirmier Puériculteur Diplômé d'état", location: 'Rabat, Maroc', sourceImageProfile: '../../assets/flower.jpg'}
 
 ]
 
@@ -18,7 +18,7 @@ const PostItem = ({ item }) => {
                 <Text>{item.postOwner}</Text>
                 <Text>{item.ownerBio}</Text>
                 <Text>{item.location}</Text>
-                
+                <Button title='Voir Profile' />
                 
             </View>
             <Image/>
