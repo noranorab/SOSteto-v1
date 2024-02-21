1bfe1423f82a3d6567719ee44c62c3734ca7620f
MeriemBelh <meriem.balhaddad2001@gmail.com>
2024-02-21 23:02:03 +0100

    update ajouter screen

--- src/screens/AjouterScreen.js
+++ src/screens/AjouterScreen.js
@@ -10,7 +10,7 @@ function InputWithIcon({ inputHeight, onPressIcon, iconName="add-outline" }) {
       style={{
         position: 'absolute',
         top: inputHeight / 2 - 12,
-        right: 26,
+        right: 25,
         marginTop: 5
       }}
     >
