2cc1f11d49a91ba43ac09d122193dc99a629c897
noranorab <noranorab7@gmail.com>
2024-02-21 14:34:23 +0100

    modified stack nav

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -24,21 +24,28 @@ const messageName = 'Ajouter'
 const parametersName = 'Paramètres'
 const notificationsName = 'Notifications'
 
-
 const Header = () => {
     const navigation = useNavigation();
-    return (
-        <Pressable onPress={() => navigation.navigate("home3") } style={{flexDirection:'row', alignItems:'center', justifyContent: 'space-between'}}>
-                            
-                            <View style={{width: '100%', marginLeft: 160, flexDirection: 'row', position: 'relative'}}>
-                                <Ionicons style={{paddingRight: 10}} name="help-circle-outline" color="black" size={28} />
-                                <Ionicons style={{paddingRight: 10}} name="notifications-outline" color="black" size={24} />
-                                <Ionicons name="person-outline" color="black" size={24} />
-                            </View>
+    
 
-                                
+    return (
+        <View style={{width: '100%', marginLeft: 160, flexDirection: 'row', position: 'relative', alignItems: 'center'}}>
+                
+                <Ionicons style={{paddingRight: 10}} name="help-circle-outline" color="black" size={28} onPress={() => {
+                navigation.navigate("home3")
+                } } />
+          
             
-        </Pressable>
+                <Ionicons style={{paddingRight: 10}} name="notifications-outline" color="black" size={24} onPress={() => {
+                navigation.navigate("home4")
+                } }/>
+
+            
+                 <Ionicons name="person-outline" color="black" size={24} onPress={() => {
+                navigation.navigate("home5")
+                } } />
+        </View>
+        
     )
 }
 
@@ -47,8 +54,21 @@ const HomeStack = createNativeStackNavigator();
 function HomeStackGroup(){
     return(
         <HomeStack.Navigator>
-            <HomeStack.Screen name="home2" component={HomeScreen}/>
+            <HomeStack.Screen name="home2" component={HomeScreen}  options={{
+                    header:() => (
+    
+                        <View style={{flexDirection:'row', alignItems:'center', justifyContent: 'space-between', paddingTop: 40, backgroundColor: 'white', marginLeft: 5}}>
+                            <Image
+                                source={require("./SOS.png")}
+                                style={{width: 100, height: 40}}
+        
+                                />
+                            <Header/>
+                        </View>
+    )}}/>
             <HomeStack.Screen name="home3" component={HomeScreen}/>
+            <HomeStack.Screen name="home4" component={HomeScreen}/>
+            <HomeStack.Screen name="home5" component={HomeScreen}/>
         </HomeStack.Navigator>
     )
 }
@@ -84,20 +104,7 @@ export default function Footer(){
                 })}
             >
                 <Tab.Screen 
-                name={homeName} component={HomeStackGroup} options={{
-                    header:() => (
-    
-                        <View style={{flexDirection:'row', alignItems:'center', justifyContent: 'space-between', paddingTop: 40, backgroundColor: 'white', marginLeft: 5}}>
-                            <Image
-                                source={require("./SOS.png")}
-                                style={{width: 100, height: 40}}
-        
-                                />
-                            <Header/>
-                        </View>
-    )}}/>
-                    
-            
+                name={homeName} component={HomeStackGroup} options={{headerShown: false}}/>
             
                 <Tab.Screen 
                 name={searchName} component={SearchScreen}/>
