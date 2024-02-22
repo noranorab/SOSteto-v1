6fcee66fdaccc824d6114fcdbcc656f8226e64ca
noranorab <noranorab7@gmail.com>
2024-02-23 00:34:31 +0100

    ajouter detail post

---
+++ src/data/infirmiers.js
@@ -0,0 +1,13 @@
+
+
+export const infirmiers = [
+    {id: 1, postOwner: 'Mohammed Boulija', ownerBio: "Infirmier Puériculteur Diplômé d'état", location: 'Rabat, Maroc', tel: '+212 639024564', sourceImageProfile: require('../../assets/flower.jpg'), actif: 'True', disponible: 'True'},
+    {id: 2, postOwner: 'Mohammed Boulija', ownerBio: "Infirmier Puériculteur Diplômé d'état", location: 'Rabat, Maroc', tel: '+212 639024564', sourceImageProfile:  require('../../assets/flower.jpg'), actif: 'True', disponible: 'False'},
+    {id: 3, postOwner: 'Mohammed Boulija', ownerBio: "Infirmier Puériculteur Diplômé d'état", location: 'Rabat, Maroc', tel: '+212 639024564', sourceImageProfile:  require('../../assets/flower.jpg'), actif: 'True', disponible: 'True'},
+    {id: 4, postOwner: 'Mohammed Boulija', ownerBio: "Infirmier Puériculteur Diplômé d'état", location: 'Rabat, Maroc', tel: '+212 639024564', sourceImageProfile:  require('../../assets/flower.jpg'), actif: 'False', disponible: 'False'},
+    {id: 5, postOwner: 'Mohammed Boulija', ownerBio: "Infirmier Puériculteur Diplômé d'état", location: 'Rabat, Maroc', tel: '+212 639024564', sourceImageProfile:  require('../../assets/flower.jpg'), actif: 'True', disponible: 'True'},
+    {id: 6, postOwner: 'Mohammed Boulija', ownerBio: "Infirmier Puériculteur Diplômé d'état", location: 'Rabat, Maroc', tel: '+212 639024564', sourceImageProfile:  require('../../assets/flower.jpg'), actif: 'True', disponible: 'True'},
+    {id: 7, postOwner: 'Mohammed Boulija', ownerBio: "Infirmier Puériculteur Diplômé d'état", location: 'Rabat, Maroc', tel: '+212 639024564', sourceImageProfile:  require('../../assets/flower.jpg'), actif: 'True', disponible: 'False'},
+    {id: 8, postOwner: 'Mohammed Boulija', ownerBio: "Infirmier Puériculteur Diplômé d'état", location: 'Rabat, Maroc', tel: '+212 639024564', sourceImageProfile:  require('../../assets/flower.jpg'), actif: 'False', disponible: 'True'}
+
+]
--- src/data/posts.js
+++ src/data/posts.js
@@ -1,13 +1,13 @@
-
+import { infirmiers } from './infirmiers'
 
 export const posts = [
-    {id: 1, postOwner: 'Mohammed Boulija', ownerBio: "Infirmier Puériculteur Diplômé d'état", location: 'Rabat, Maroc', sourceImageProfile: require('../../assets/flower.jpg'), actif: 'True'},
-    {id: 2, postOwner: 'Mohammed Boulija', ownerBio: "Infirmier Puériculteur Diplômé d'état", location: 'Rabat, Maroc', sourceImageProfile:  require('../../assets/flower.jpg'), actif: 'True'},
-    {id: 3, postOwner: 'Mohammed Boulija', ownerBio: "Infirmier Puériculteur Diplômé d'état", location: 'Rabat, Maroc', sourceImageProfile:  require('../../assets/flower.jpg'), actif: 'True'},
-    {id: 4, postOwner: 'Mohammed Boulija', ownerBio: "Infirmier Puériculteur Diplômé d'état", location: 'Rabat, Maroc', sourceImageProfile:  require('../../assets/flower.jpg'), actif: 'False'},
-    {id: 5, postOwner: 'Mohammed Boulija', ownerBio: "Infirmier Puériculteur Diplômé d'état", location: 'Rabat, Maroc', sourceImageProfile:  require('../../assets/flower.jpg'), actif: 'True'},
-    {id: 6, postOwner: 'Mohammed Boulija', ownerBio: "Infirmier Puériculteur Diplômé d'état", location: 'Rabat, Maroc', sourceImageProfile:  require('../../assets/flower.jpg'), actif: 'True'},
-    {id: 7, postOwner: 'Mohammed Boulija', ownerBio: "Infirmier Puériculteur Diplômé d'état", location: 'Rabat, Maroc', sourceImageProfile:  require('../../assets/flower.jpg'), actif: 'True'},
-    {id: 8, postOwner: 'Mohammed Boulija', ownerBio: "Infirmier Puériculteur Diplômé d'état", location: 'Rabat, Maroc', sourceImageProfile:  require('../../assets/flower.jpg'), actif: 'False'}
+    {id: 1, infirmier: infirmiers[0], postOwner: 'Mohammed Boulija', ownerBio: "Infirmier Puériculteur Diplômé d'état", location: 'Rabat, Maroc', sourceImageProfile: require('../../assets/flower.jpg'), actif: 'True'},
+    {id: 2, infirmier: infirmiers[1], postOwner: 'Mohammed Boulija', ownerBio: "Infirmier Puériculteur Diplômé d'état", location: 'Rabat, Maroc', sourceImageProfile:  require('../../assets/flower.jpg'), actif: 'True'},
+    {id: 3, infirmier: infirmiers[2], postOwner: 'Mohammed Boulija', ownerBio: "Infirmier Puériculteur Diplômé d'état", location: 'Rabat, Maroc',  sourceImageProfile:  require('../../assets/flower.jpg'), actif: 'True'},
+    {id: 4, infirmier: infirmiers[3],postOwner: 'Mohammed Boulija', ownerBio: "Infirmier Puériculteur Diplômé d'état", location: 'Rabat, Maroc', sourceImageProfile:  require('../../assets/flower.jpg'), actif: 'False'},
+    {id: 5, infirmier: infirmiers[4],postOwner: 'Mohammed Boulija', ownerBio: "Infirmier Puériculteur Diplômé d'état", location: 'Rabat, Maroc', sourceImageProfile:  require('../../assets/flower.jpg'), actif: 'True'},
+    {id: 6, infirmier: infirmiers[5],id_infirmier: 6,postOwner: 'Mohammed Boulija', ownerBio: "Infirmier Puériculteur Diplômé d'état", location: 'Rabat, Maroc', sourceImageProfile:  require('../../assets/flower.jpg'), actif: 'True'},
+    {id: 7, infirmier: infirmiers[6],id_infirmier: 7,postOwner: 'Mohammed Boulija', ownerBio: "Infirmier Puériculteur Diplômé d'état", location: 'Rabat, Maroc', sourceImageProfile:  require('../../assets/flower.jpg'), actif: 'True'},
+    {id: 8, infirmier: infirmiers[7],id_infirmier: 8,postOwner: 'Mohammed Boulija', ownerBio: "Infirmier Puériculteur Diplômé d'état", location: 'Rabat, Maroc', sourceImageProfile:  require('../../assets/flower.jpg'), actif: 'False'}
 
 ]
